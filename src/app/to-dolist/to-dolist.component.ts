4f801a5eae0ea5dddddbcc8bf1829e727789efa4
Nanik1040 <43468978+Nanik1040@users.noreply.github.com>
2023-08-11 18:39:56 +0530

    Initial commit

---
+++ src/app/to-dolist/to-dolist.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-to-dolist',
+  templateUrl: './to-dolist.component.html',
+  styleUrls: ['./to-dolist.component.css'],
+})
+export class ToDolistComponent implements OnInit {
+  constructor() {}
+  //using inpur decorator
+  @Input() todos: string[];
+  //using viewchild
+  // todos: string[] = [];
+  // addTodoItem(todo: string) {
+  //   this.todos.unshift(todo);
+  // }
+  @Output() messageEvent = new EventEmitter<string>();
+
+  sendMessage() {
+    console.log('logging');
+    this.messageEvent.emit('Hello from child component!');
+  }
+  ngOnInit() {}
+  deleteTodoItem(todo: string) {
+    const index = this.todos.indexOf(todo);
+    // if (index !== -1) {
+    this.todos.splice(index, 1);
+    // }
+  }
+}
